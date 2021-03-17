5e03045b79526bcdb1cb8b977486df5a506a35c8
Carlos Costa <carloscosta77@gmail.com>
2021-03-17 23:12:02 +0000

    Commit

--- userapi/database/seeders/UsersTableSeeder.php
+++ userapi/database/seeders/UsersTableSeeder.php
@@ -17,21 +17,19 @@ class UsersTableSeeder extends Seeder
         // Truncate existing records to start from scratch
         \App\Models\User::truncate();
 
-
-#        $faker = \Faker\Factory::create();
- #       $faker->addProvider(new \Faker\Provider\en_US\Person($faker));
+/*        
+        $faker = \Faker\Factory::create();
+        $faker->addProvider(new \Faker\Provider\en_US\Person($faker));
 
         // Create some users
-#        for ($i = 0; $i < 10; $i++) {
-#           User::create([
-#              'name' => $faker->name,
-#                'email' => $faker->email,
-#                'birthday' => $faker->dateTimeThisCentury->format('Y-m-d'),
-#                'gender' => $faker->randomElement($array = array ('m','f')),
-#            ]);
-#        }
-
-
+        for ($i = 0; $i < 10; $i++) {
+           User::create([
+              'name' => $faker->name,
+                'email' => $faker->email,
+                'birthday' => $faker->dateTimeThisCentury->format('Y-m-d'),
+                'gender' => $faker->randomElement($array = array ('m','f')),
+            ]);
+        }
+*/                
     }
-
 }
